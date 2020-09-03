b31a4818a21ca0819ac37d80306d78c45ccf8816
gdreid13 <gdreid13@gmail.com>
2020-09-03 15:38:53 -0700

    fixing build issues

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: process.env.API_ENDPOINT,
+  API_ENDPOINT: 'https://shrouded-hamlet-24502.herokuapp.com/',
   TOKEN_KEY: 'hybowar-client-auth-token',
   USER: 'user',
   USER_ID: 'user_id',
